04dc113013f248e99ca67137c6c96b9e6dddbcef
miccajr <56576315+miccajr@users.noreply.github.com>
2020-10-17 14:02:37 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="shortcut icon" href="image/dafunda-fav.png">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+	<title>Dafunda.com - Media Geek dan Pop Culture Terbaik di Indonesia</title>
+	<!-- Template Main CSS File -->
+	<link href="/css/main.css" rel="stylesheet">
+
+</head>
+<style type="text/css">
+	
+
+	/* General */
+	.main {
+		width: 100%;
+	}
+
+	.item {
+		border: 2px solid #B85B14;
+		width: 120px;
+	}
+
+
+	/* Flex */
+	.flex {
+		display: flex;
+		flex-wrap: nowrap; 
+		overflow: hidden;
+		height: 60px;
+	}
+
+	.flex:hover {
+		overflow: auto;
+		overflow-y: hidden; /* Hide vertical scrollbar */
+	}
+
+	.flex-item {
+		flex: 0 0 auto;
+		margin-top: 5px;
+	}
+
+	.btn-tag {
+		border-radius: 20px;
+		background: transparent;
+		margin: 10px;
+		font-size: 1.19vw;
+		color: black;
+		padding: 10px;
+		padding-top: 5px;
+		padding-bottom: 5px;
+		font-size: 1.11vw;
+	}
+
+	.btn-tag:focus {
+		outline: 0;
+	}
+
+	body {
+		overflow-x: hidden; /* Hide horizontal scrollbar */
+	}
+</style>
+<body>
+	<!-- Header -->
+	<div class="fixed-top border" style="width: 100%; background: white; height: 10.7%;">
+		<nav class="navbar navbar-expand-lg navbar-light" style="padding-top: 0.85%; margin-left: 83px;">
+			<a class="navbar-brand" href="#">
+				<img src="image/dafunda-header.png" style="width: 100%; height: auto;">
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent" style="font-family: Segoe UI; float: left;">
+				<ul class="navbar-nav mr-auto">				
+					<li class="nav-item dropdown" style="margin-right: 20px;">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: black; font-size: 1.4vw; font-weight: 900;">
+							#HOME
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item font-weight-bold" href="#" style="font-size: 0.95vw;">TENTANG KAMI</a>
+							<a class="dropdown-item font-weight-bold" href="#" style="font-size: 0.95vw;">KEBIJAKAN PRIVASI</a>
+							<a class="dropdown-item font-weight-bold" href="#" style="font-size: 0.95vw;">KETENTUAN LAYANAN</a>
+							<a class="dropdown-item font-weight-bold" href="#" style="font-size: 0.95vw;">KONTAK & ALAMAT</a>
+							<a class="dropdown-item font-weight-bold" href="#" style="font-size: 0.95vw;">PASANG IKLAN</a>
+							<a class="dropdown-item font-weight-bold" href="#" style="font-size: 0.95vw;">F.A.Q</a>
+						</div>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#" style="color: black; font-size: 1.4vw; font-weight: 700;">Games</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#" style="color: black; font-size: 1.4vw; font-weight: 700;">Movies</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#" style="color: black; font-size: 1.4vw; font-weight: 700;">Komik</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#" style="color: black; font-size: 1.4vw; font-weight: 700;">Tv</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#" style="color: black; font-size: 1.4vw; font-weight: 700;">AniManga</a>
+					</li>
+				</ul>
+				<button class="font-weight-bold" style="background-color: #ecf0f1; font-size: 0.6vw; border-radius: 20px; padding: 10px; border: transparent; margin-right: 20px;">MODE MALAM</button>
+				<button style="background: transparent; margin-right: 10px; border: transparent;"><i class="fas fa-share-alt"></i></button>
+				<button style="border: transparent; border-radius: 20px; padding: 5px; padding-right: 10px; padding-left: 10px; margin-right: 83px;"><i class="fas fa-search"></i></button>
+			</div><br>
+		</nav>
+	</div>
+	<!-- End Header -->
+
+	<!-- Tag Header -->
+	<div class="main">
+		<div class="flex" style="margin-top: 6%;">
+			<div class="flex-item"><a href="#Pop" class="border btn-tag"><b style="font-weight: 800;">#</b> Pop Culture</a></div>
+			<div class="flex-item"><a href="#Dafunda Game" class="border btn-tag"><b style="font-weight: 800;">#</b> Dafunda Game</a></div>
+			<div class="flex-item"><a class="border btn-tag"><b style="font-weight: 800;">#</b> Dafunda Movie</a></div>
+			<div class="flex-item"><a class="border btn-tag"><b style="font-weight: 800;">#</b> Dafunda TV</a></div>
+			<div class="flex-item"><a class="border btn-tag"><b style="font-weight: 800;">#</b> Dafunda Komik</a></div>
+			<div class="flex-item"><a class="border btn-tag"><b style="font-weight: 800;">#</b> Dafunda Tekno</a></div>
+			<div class="flex-item"><a class="border btn-tag"><b style="font-weight: 800;">#</b> Dafunda Otaku</a></div>
+			<div class="flex-item"><a class="border btn-tag"><b style="font-weight: 800;">#</b> Dafunda Viral</a></div>
+			<div class="flex-item"><a class="border btn-tag"><b style="font-weight: 800;">#</b> Dafunda Musik</a></div>
+			<div class="flex-item"><a class="border btn-tag"><b style="font-weight: 800;">#</b> Dafunda eSports</a></div>
+			<div class="flex-item"><a class="border btn-tag"><b style="font-weight: 800;">#</b> Dafunda Cara</a></div>
+			<div class="flex-item"><a class="border btn-tag"><b style="font-weight: 800;">#</b> Dafunda Gadget</a></div>
+			<div class="flex-item"><a class="border btn-tag"><b style="font-weight: 800;">#</b> Gamedaim</a></div>
+			<div class="flex-item"><a class="border btn-tag"><b style="font-weight: 800;">#</b> CaraGame.id</a></div>
+			<div class="flex-item"><a class="border btn-tag"><b style="font-weight: 800;">#</b> Wowkia Media</a></div>
+		</div>
+	</div>
+	<!-- End Tag Header -->
+
+	<!-- Slide -->
+	<div id="carouselExampleControls" class="carousel slide" data-ride="carousel" style="width: 800px;">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img class="d-block w-100" src="image/news1.jpg" alt="First slide" style="height: 400px;">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="image/news2.jpg" alt="Second slide" style="height: 400px;">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="image/news3.jpg" alt="Third slide" style="height: 400px;">
+			</div>
+		</div>
+		<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+	<!-- End Slide -->
+
+	<!-- Content -->
+	<div class="container-fluid" style="width: 90%; height: 800px; margin-top: 40px;">
+		<span class="font-weight-bold" style="font-size: 1.5vw; text-align: left;">DAFUNDA LATEST</span>
+		<div class="row">
+			<div class="col-10">
+				<span class="font-weight-bold border" style="font-size: 1.5vw; float: left; margin-top: 1%; border-radius: 5px;">
+					<img src="image/news1.jpg" style="width: 30%; height: auto; border-radius: 10px; float: left;">
+					<span style="margin-left: 2%;">Bad Genius 2 Dikabarkan Akan Dirilis Tahun 2020!!<br>
+						<img src="image/miccajr.jpg" style="border-radius: 50%; width: 5%; height: auto; top: 0; float: left; margin-left: 2%;">
+						<span class="font-weight-normal" style="font-size: 0.8vw; color: #c4c4c2; float: left; padding-left: 1%;">oleh 
+							<span style="color: black;">Micca JR
+							</span><br>sekitar 2 jam yang lalu
+						</span>
+					</span>
+				</span>
+			</div>
+			<div class="col-10">
+				<span class="font-weight-bold border" style="font-size: 1.5vw; float: left; margin-top: 3%; border-radius: 5px;">
+					<img src="image/news2.jpg" style="width: 30%; height: auto; border-radius: 10px; float: left;">
+					<span style="margin-left: 2%;">Film Alita mendapatkan Reward Hollywood!!<br>
+						<img src="image/miccajr.jpg" style="border-radius: 50%; width: 5%; height: auto; top: 0; float: left; margin-left: 2%;">
+						<span class="font-weight-normal" style="font-size: 0.8vw; color: #c4c4c2; float: left; padding-left: 1%;">oleh 
+							<span style="color: black;">Micca JR
+							</span><br>sekitar 6 jam yang lalu
+						</span>
+					</span>
+				</span>
+			</div>
+			<div class="col-10">
+				<span class="font-weight-bold border" style="font-size: 1.5vw; float: left; margin-top: 3%; border-radius: 5px;">
+					<img src="image/news3.jpg" style="width: 30%; height: auto; border-radius: 10px; float: left;">
+					<span style="margin-left: 2%;">Violet Evergarden Resmi Mendapatkan Movie Ke-2nya!!<br>
+						<img src="image/miccajr.jpg" style="border-radius: 50%; width: 5%; height: auto; top: 0; float: left; margin-left: 2%;">
+						<span class="font-weight-normal" style="font-size: 0.8vw; color: #c4c4c2; float: left; padding-left: 1%;">oleh 
+							<span style="color: black;">Micca JR
+							</span><br>sekitar 11 jam yang lalu
+						</span>
+					</span>
+				</span>
+			</div>
+			<div class="col-2">
+				2 of 2
+			</div>
+		</div>
+		<!-- End Content -->
+
+		<!-- Optional JavaScript; choose one of the two! -->
+
+		<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+	</body>
+	</html>
